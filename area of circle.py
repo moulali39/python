90d2e6e64e0c2ca509ed7b86d72b7ad156328325
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-08-02 22:17:02 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,35 @@
+# PI=3.14
+# r=float(input("enter the radious of circle:"))
+# area= PI * r * r
+# print("area of a circle = %.2f" %area)
+
+
+# """""RADIOUS OF CIRCLE PRACTICE PROGRAMME""""""
+
+
+
+# r=float(input("enter the radious of circle:"))
+# PI=22/7
+# area=  PI * r * r
+# print("area of circle is %.2f" %area)    # 314.29
+
+
+
+
+# """""""""   cube values    """""""""""""
+
+# n=float(input("enter the value of n:"))
+# print("cube of the value is:" ,n**3)
+
+
+
+# """""""""""""area of circle""""""""
+
+
+# r=float(input("enter the value of r:"))
+# PI=22/7
+# area= PI * r * r
+# print("area of circle is %.2f" %area)
+
+
+
